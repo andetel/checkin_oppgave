3b0293506540dacf3f784587381a174271db3295
andetel <anders1.tellefsen@gmail.com>
2023-08-31 18:23:26 +0200

    Created Starship and Vehicle interfaces

---
+++ src/interfaces/Starship.tsx
@@ -0,0 +1,9 @@
+export interface Starship {
+    crew: string,
+    cost: Number,
+    maxAtmospheringSpeed: Number,
+    hyperdriveRating: Number,
+    cargoCapacity: Number,
+    length: Number,
+    manufacturers: Array<string>
+}
---
+++ src/interfaces/Vehicle.tsx
@@ -0,0 +1,8 @@
+export interface Vehicle {
+    crew: string,
+    cost: Number,
+    maxAtmospheringSpeed: Number,
+    cargoCapacity: Number,
+    length: Number,
+    manufacturers: Array<string>
+}
