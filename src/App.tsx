24109bcf1fb61bb1cb1f4f4c2fc1c128f7264fec
andetel <anders1.tellefsen@gmail.com>
2023-09-04 22:16:32 +0200

    Made some minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import {Navbar} from "./components/Navbar";
 import {Card} from "./components/Card";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-        <Navbar />
-        <Card />
-    </div>
-  );
+const App = () => {
+    return (
+        <div className="App">
+            <Navbar />
+            <Card />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/CardList.tsx
@@ -0,0 +1,5 @@
+export const CardList = () => {
+    return (
+        <h1>Card list!</h1>
+    )
+}
